c0df4077982116d403d7b00d8a84a6681de225d3
Eddy <eddytougon@gmail.com>
2023-04-24 18:22:11 +0100

    style: ajuement header notfound et card

--- src/Components/header/header.js
+++ src/Components/header/header.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import './header.css'
 import PropTypes from 'prop-types';
+import { Link, useLocation } from 'react-router-dom';
+
 function Header(props) {
+  const location = useLocation();
+  console.log(location.pathname); 
+
   return (
     <header className="header">
       <a href={props.logolink}><img src={props.logoSrc} alt="Logo" className="logo" /></a>
       <nav>
-          <a href="../">Accueil</a>
-          <a href="../APropos">À propos</a>
+        <a className={`home ${location.pathname === '/' ? 'active' : ''}`} href="../">Accueil</a>
+        <a className={`about ${location.pathname === '/APropos' ? 'active' : ''}`}  href="../APropos">À propos</a>
       </nav>
     </header>
   );
 }
+
 Header.propTypes =  {
-  logolink:PropTypes.string,
-  logoSrc:PropTypes.string
+  logolink: PropTypes.string,
+  logoSrc: PropTypes.string
 }
+
 export default Header;
